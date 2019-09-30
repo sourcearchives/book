75e7e3c50c000d9e13cb643dc75cf43a5ba17267
David Revoy <info@davidrevoy.com>
2019-09-30 21:41:36 +0200

    Rewrite GetPages to be compatible with new remote downloader
    Compatible with new website:
    - Query and download by seasons.
    - Wait for the server for CPU friendlyness.
    
    More user friendly:
    - Humor.
    - More color.
    - Better and short descriptive sentence during the process.
    
    Failproof
    - Studied scenario for it to fail, workaround that.
    - Validations when things arrive.
    - Add messages to guide user when things are failing.
    
    More services:
    - Create folders when missing.
    - Create a proof and check the zip is deflated correctly.
    
    For fork and branches: I recommend to cherry-pick this one.
    Breaking down the full repo by season also makes the directory
    less heavy to work on only book one.
    
    Ping: https://framagit.org/peppercarrot/book/merge_requests/10

--- getPages.bash
+++ getPages.bash
@@ -8,6 +8,16 @@
 # author: laurent valentin jospin (laurent.valentin@famillejospin.ch) ; david revoy (info@davidrevoy.com)
 # license: public domain
 
+# configuration
+# -------------
+# For lang available, check at least the one with ep1 translated here: https://www.peppercarrot.com/0_sources/ep01_Potion-of-Flight/lang/
+lang="en"
+# Season 1: from ep01 → ep11
+# Season 2: from ep12 → ep21
+# Season 3: from ep22 → ep29
+# Season 4: from ep30 → (wip...)
+season="1"
+
 clear
 
 # colors
@@ -16,34 +26,67 @@ Red=$'\e[1;31m'
 Green=$'\e[1;32m'
 Blue=$'\e[1;34m'
 
-# configuration
-lang="en"
 if [[ "$1" =~ ^[a-z]{2}$ ]]; then
    lang="$1"
 fi
 
-file="$(date --utc +%Y-%m-%d)_${lang}_peppercarrot-all-pages.zip"
+file="$(date --utc +%Y-%m-%d)_${lang}_S0${season}_peppercarrot-all-pages.zip"
 urlpath="https://www.peppercarrot.com/tmp/${file}"
-workingpath="${PWD}/lang/${lang}"
+path="${PWD}/lang/${lang}"
 
-if [ ! -d "$workingpath"/"pages" ]; then
-   echo "${Red}=> Error : No directory -pages- found. Please execute the script in the sources directory. ${Off}"
+# Block if executed not at root of directory.
+if [ ! -d "lang" ]; then
+  if [ ! -f "README.md" ]; then
+   echo "${Red}=> Error :${Off} Can't find lang/ or README.md!"
+   echo "Are you executing that at the root of the repo?"
+   echo "Please try again."
    exit
+  fi
 fi
 
-echo "${Green}=> Creating image cache to download in step 2. ${Off}"
-curl "https://www.peppercarrot.com/downloader.php?l=${lang}" &> /dev/null
+# Create the target folder
+if [ ! -d "${path}/pages" ]; then
+    echo "${Green}=>Missing directory for this lang: creating folder ${path}/pages ${Off}"
+    mkdir -p "${path}/pages"
+    if [ ! -d "${path}/pages" ]; then
+      echo "${Red}=>Error: coudn't create directory. Please check permissions. ${Off}"
+      exit
+    fi
+fi
 
+echo "${Green}=> Query Pepper&Carrot website${Off}"
+echo "${Blue}Calling the 'zip download generator' for getting a fresh pack.${Off}"
+
+curl "https://www.peppercarrot.com/downloader.php?l=${lang}&s=${season}" &> /dev/null
+echo "${Blue}Call done.${Off}"
+
+echo ""
+echo "${Blue}  ...Waiting 30sec to let the time for the server to generate${Off}"
+echo "${Blue}  the big zip files. It prepares a 200MB zip with hundreds of PNGs inside.${Off}"
+sleep 10
+echo "${Blue}  ...20sec${Off}"
+sleep 10
+echo "${Blue}  ...10sec... Almost done!${Off}"
+sleep 10
+echo "${Blue}  ...${Off}${Green}=>GO!${Off}"
 echo ""
 
 echo "${Green}=> Downloading the [$lang] archive from peppercarrot.com .${Off}"
-echo "${Blue}   Note: it's a 500MB download, it can be long.${Off}"
+echo "${Blue}   Note: it's a 200MB download, it can be long.${Off}"
+echo ""
 wget --continue --output-document="/tmp/${file}" "${urlpath}"
 
 echo ""
 
+# Check if we have the archive.
 if [ -f "/tmp/${file}" ]; then
-   unzip -o "/tmp/${file}" -d "$workingpath"/"pages"
+   # Remove previous README.md files before unzipping
+   if [ -f "${path}/pages/README.md" ]; then
+      rm "${path}/pages/README.md"
+   fi
+   echo "${Green}=> Unzipping the archive .${Off}"
+   echo ""
+   unzip -o "/tmp/${file}" -d "$path"/"pages"
    echo "${Green}=> Unzip archive done. ${Off} "
    rm "/tmp/${file}"
 else
@@ -52,13 +95,16 @@ fi
 
 echo ""
 
-if [ -f "${workingpath}/pages/${lang}_Pepper-and-Carrot_by-David-Revoy_E01P00.png" ]; then
-   echo "=> Test: ${Green}Success !${Off} All is done."
+# We check for the presence of the new README.md inside the deflated archive.
+# As we removed it before, this system ensure the work was done.
+if [ -f "${path}/pages/README.md" ]; then
+   echo "=> Quick install test: ${Green}Success !${Off}"
+   echo "${Blue} All looks correctly setup.${Off}"
    echo ""
 else
-   echo "=> Test: ${Red} Error. Check test failed. ${Off} "
+   echo "=> Quick install test: ${Red} Error. Check test failed. ${Off} "
    echo "   Something didn't work in the process... Please report a bug here: https://github.com/Deevad/peppercarrot_print_book01/issues "
-   echo "   Debug: ""$workingpath"/"pages"
+   echo "   Debug: ""$path"/"pages"
    echo ""
 fi
 
